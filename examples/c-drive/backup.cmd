e211cfc8cea546d05952299254cadff6d2efbe56
Michael Hopcroft <mhop@microsoft.com>
2023-04-02 13:10:11 -0700

    Example scripts

---
+++ examples/c-drive/backup.cmd
@@ -0,0 +1,11 @@
+setlocal
+cd /D "%~dp0"
+rem call backup-one-folder.cmd C:\backups\mhoplenovo2-1-27-18 mhoplenovo2-1-27-18
+rem call backup-one-folder.cmd C:\Data\gov2 gov2
+rem call backup-one-folder.cmd C:\sigir sigir
+rem call backup-one-folder.cmd "C:\Users\mhop\Creative Cloud Files" "Creative Cloud Files"
+call backup-one-folder.cmd C:\Users\mhop\Documents Documents
+call backup-one-folder.cmd C:\Users\mhop\Pictures Pictures
+call backup-one-folder.cmd C:\Users\mhop\OneDrive OneDrive
+call backup-one-folder.cmd "C:\Users\mhop\OneDrive - Microsoft" "OneDrive - Microsoft"
+rem call backup-one-folder.cmd C:\temp temp
---
+++ examples/d-drive/backup.cmd
@@ -0,0 +1,21 @@
+setlocal
+cd /D "%~dp0"
+rem call backup-one-folder.cmd C:\backups\mhoplenovo2-1-27-18 mhoplenovo2-1-27-18
+rem call backup-one-folder.cmd C:\Data\gov2 gov2
+rem call backup-one-folder.cmd C:\sigir sigir
+rem call backup-one-folder.cmd "C:\Users\mhop\Creative Cloud Files" "Creative Cloud Files"
+rem call backup-one-folder.cmd C:\Users\mhop\Documents Documents
+rem call backup-one-folder.cmd C:\Users\mhop\Pictures Pictures
+rem call backup-one-folder.cmd C:\Users\mhop\OneDrive OneDrive
+rem call backup-one-folder.cmd "C:\Users\mhop\OneDrive - Microsoft" "OneDrive - Microsoft"
+
+rem call backup-one-folder.cmd d:\backups backups
+rem call backup-one-folder.cmd d:\bitlocker bitlocker
+
+
+rem call backup-one-folder.cmd d:\data data
+rem call backup-one-folder.cmd d:\git git
+
+rem call backup-one-folder.cmd d:\sigir sigir
+call backup-one-folder.cmd d:\temp temp
+rem call backup-one-folder.cmd d:\wsl wsl
