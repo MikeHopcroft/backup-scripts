78041327be425d92b9dbe6ab57c39e1a55e1de32
Michael Hopcroft <mhop@microsoft.com>
2023-04-02 13:12:49 -0700

    Example backs up ssh keys

--- examples/c-drive/backup.cmd
+++ examples/c-drive/backup.cmd
@@ -4,8 +4,9 @@ rem call backup-one-folder.cmd C:\backups\mhoplenovo2-1-27-18 mhoplenovo2-1-27-1
 rem call backup-one-folder.cmd C:\Data\gov2 gov2
 rem call backup-one-folder.cmd C:\sigir sigir
 rem call backup-one-folder.cmd "C:\Users\mhop\Creative Cloud Files" "Creative Cloud Files"
-call backup-one-folder.cmd C:\Users\mhop\Documents Documents
-call backup-one-folder.cmd C:\Users\mhop\Pictures Pictures
-call backup-one-folder.cmd C:\Users\mhop\OneDrive OneDrive
-call backup-one-folder.cmd "C:\Users\mhop\OneDrive - Microsoft" "OneDrive - Microsoft"
+call backup-one-folder.cmd C:\Users\mhop\.ssh .ssh
+rem call backup-one-folder.cmd C:\Users\mhop\Documents Documents
+rem call backup-one-folder.cmd C:\Users\mhop\Pictures Pictures
+rem call backup-one-folder.cmd C:\Users\mhop\OneDrive OneDrive
+rem call backup-one-folder.cmd "C:\Users\mhop\OneDrive - Microsoft" "OneDrive - Microsoft"
 rem call backup-one-folder.cmd C:\temp temp
